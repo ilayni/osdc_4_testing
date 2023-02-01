6fee0ba65c1c9c20f75f5b3704d8a912a86004eb
ilayni <ilayorion@gmail.com>
2023-02-01 08:35:23 +0200

    updating tests pip install pytest-random-order Pip install pytest-cover
    Wsl sees old version of pytest
    py.test --version
    Need to run
    python3 -m pytest test_myMath.py --cov --cov-report html --cov-report term
    Also --random-order with python3 -m

--- test_myMath.py
+++ test_myMath.py
@@ -1,4 +1,11 @@
 import myMath
 
+def test_zero():
+    assert myMath.add(0,0) == 0
+
 def test_add():
+    assert myMath.add(2,2) == 4
+    assert myMath.add(3,3) == 6
+
+def test_add_2():
     assert myMath.add(2,2) == 4
