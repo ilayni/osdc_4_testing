0f1b4c729e512bc57c0e47d8ceac9af198910afb
ilayni <ilayorion@gmail.com>
2023-02-01 07:55:51 +0200

    adding test with pytest and using amend

--- myMath.py
+++ myMath.py
@@ -1,6 +1,8 @@
 
-def mul(x:int, y:int) -> int:
+#def mul(x:int, y:int) -> int:
+def mul(x, y):
     return x+y
 
-def add(x:int, y:int) -> int:
+#def add(x:int, y:int) -> int:
+def add(x, y):
     return x*y
---
+++ test_myMath.py
@@ -0,0 +1,4 @@
+import myMath
+
+def test_add():
+    assert myMath.add(2,2) == 4
